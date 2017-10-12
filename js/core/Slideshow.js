2d071bb1665895e2901353d95070a5899ea04180
qualitymonth <31244960+qualitymonth@users.noreply.github.com>
2017-10-12 11:02:43 -0400

    Update Slideshow.js

--- js/core/Slideshow.js
+++ js/core/Slideshow.js
@@ -36,7 +36,22 @@ function Slideshow(config){
 	//////////////////////////////////////////////////
 	/////////////// GO TO NEXT SLIDE /////////////////
 	//////////////////////////////////////////////////
-
+		if (!String.prototype.includes) {
+  String.prototype.includes = function(search, start) {
+    'use strict';
+    if (typeof start !== 'number') {
+      start = 0;
+    }
+    
+    if (start + search.length > this.length) {
+      return false;
+    } else {
+      return this.indexOf(search, start) !== -1;
+    }
+  };
+}
+	
+	
 	// Go to next slide
 	self.nextSlide = function(){
 
